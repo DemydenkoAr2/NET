ba7d1ae12e6c8f7659c3c05bd8356460385f7cf7
Artem <mxnxchrxmx1@gmail.com>
2025-03-02 20:14:38 -1000

    HW

---
+++ HomeWork_2/HW_DecimalNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HW_DecimalNumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DecimalNumber n = new DecimalNumber(42.55m);
+        
+        n.SplitParts();
+        n.Print();
+    }
+}
---
+++ HomeWork_2/HW_MusicalInstrument/Program.cs
@@ -0,0 +1,20 @@
+﻿using HW_MusicalInstrument.Instruments;
+
+namespace HW_MusicalInstrument;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<MusicalInstrument> instruments = [new Cello(), new Trombone(), new Ukulele(), new Violin()];
+        
+        foreach (var instrument in instruments)
+        {
+            instrument.Show();
+            instrument.Sound();
+            instrument.Desc();
+            instrument.History();
+            Console.WriteLine();
+        }
+    }
+}
