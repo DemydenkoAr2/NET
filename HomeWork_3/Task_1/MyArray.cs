f66612464188b27951947bee22525b537e87446a
Artem <mxnxchrxmx1@gmail.com>
2025-03-04 20:14:11 -1000

    Homework - 3 | Task - 1

---
+++ HomeWork_3/Task_1/MyArray.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_1;
+
+public class MyArray : IOutput
+{
+    private int[] _array;
+
+    public MyArray(int[] array)
+    {
+        _array = array;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Array's elements: ");
+        foreach (var item in _array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine(info);
+        Console.WriteLine("Array's elements: ");
+
+        foreach (var item in _array)
+        {
+            Console.Write(item + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ HomeWork_3/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        MyArray myArray = new MyArray(arr);
+        
+        myArray.Show();
+        myArray.Show("Some additional information");
+    }
+}
