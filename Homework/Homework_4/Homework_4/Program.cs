cfe88d4f7aa6b5c22c8a1737c0f731aec38f2d8f
Artem <mxnxchrxmx1@gmail.com>
2025-03-09 13:22:37 -1000

    HW - 4 | Task - 1

---
+++ Homework/Homework_4/Homework_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee employeeArtem = new Employee("Artem", "QA", 1200m);
+        Employee employeeAlex = new Employee("Alex", "QA Automation", 1600m);
+
+        employeeArtem += 400;
+        employeeAlex -= 200;
+
+        employeeArtem.GetInfo();
+        employeeAlex.GetInfo();
+
+        Console.WriteLine(employeeArtem > employeeAlex);
+        Console.WriteLine(employeeArtem < employeeAlex);
+        Console.WriteLine(employeeArtem == employeeAlex);
+        Console.WriteLine(employeeArtem != employeeAlex);
+    }
+}
