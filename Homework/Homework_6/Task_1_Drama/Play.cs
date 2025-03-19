b5af69c6f3144993282bc1299163230ac0ef1a77
Artem <mxnxchrxmx1@gmail.com>
2025-03-19 12:31:04 -1000

    Homework - 6 - Task 1, 2

---
+++ Homework/Homework_6/Task_1_Drama/Play.cs
@@ -0,0 +1,50 @@
+﻿namespace Task_1_Drama;
+
+public class Play : IDisposable
+{
+    private string Title { get; set; }
+    private string Author { get; set; }
+    private string Genre { get; set; }
+    private int Year { get; set; }
+
+    public Play(string title, string author, string genre, int year)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+        Console.WriteLine($"Play \"{Title}\" created.");
+    }
+
+    // Destructor
+    ~Play()
+    {
+        Console.WriteLine($"Play \"{Title}\" destroyed. Author: {Author}, genre: {Genre}, year: {Year}");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"П'єса \"{Title}\" видалена через Dispose.");
+        GC.SuppressFinalize(this); // Відміна виклику деструктора
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Genre: {Genre}");
+        Console.WriteLine($"Year: {Year}");
+    }
+
+    public void ChangeTitle(string newTitle)
+    {
+        Console.WriteLine($"Title changed from \"{Title}\" to \"{newTitle}\".");
+        Title = newTitle;
+    }
+
+    public void ChangeYear(int newYear)
+    {
+        Console.WriteLine($"Year changed from {Year} to {newYear}.");
+        Year = newYear;
+    }
+}
