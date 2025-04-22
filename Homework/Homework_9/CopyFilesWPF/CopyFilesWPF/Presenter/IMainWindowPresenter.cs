05f1d8433a3229d3849ebbd3ee876325d08e292f
Artem <mxnxchrxmx1@gmail.com>
2025-04-22 12:07:13 -0700

    Homework SQL

---
+++ Homework/Homework_9/CopyFilesWPF/CopyFilesWPF/Presenter/IMainWindowPresenter.cs
@@ -0,0 +1,9 @@
+﻿namespace CopyFilesWPF.Presenter
+{
+    public interface IMainWindowPresenter
+    {
+        void CopyButtonClick();
+        void ChooseFileFromButtonClick(string path);
+        void ChooseFileToButtonClick(string path);
+    }
+}
